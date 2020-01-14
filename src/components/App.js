9b48de2f6de6a4f0ace8d1abe5571f50aaa0fcbc
Vladimir Tkachenko <vladimir.tkachenko@progforce.com>
2020-01-14 16:44:48 +0300

    10: refactor: change basket store from Obj to Map

--- src/components/App.js
+++ src/components/App.js
@@ -10,8 +10,8 @@ import "../css/App.css";
 
 export function App() {
   const [showCategory, changeShowCategory] = useState("sandwiches");
-  const [basket, changeBasket] = useState({ total: 0 });
-
+  const [basket, changeBasket] = useState(new Map());
+  const [totalBasket, changeTotalBasket] = useState(0);
   const state = {
     showCategory,
     basket
@@ -22,27 +22,24 @@ export function App() {
   }
 
   function addToBasket(nameProduct, amount, price) {
-    let newbasketItem = {
-      [nameProduct]: [nameProduct, amount, price],
-      total: +state.basket.total + +amount * +price
-    };
-
-    if (!state.basket.hasOwnProperty(nameProduct)) {
-      changeBasket({ ...state.basket, ...newbasketItem });
+    if (!state.basket.has(nameProduct)) {
+      state.basket.set(nameProduct, { amount, price });
     } else {
-      newbasketItem[nameProduct][1] = state.basket[nameProduct][1] + amount;
-      changeBasket({ ...state.basket, ...newbasketItem });
+      let { amount: newAmount } = state.basket.get(nameProduct);
+      newAmount = newAmount + amount;
+      state.basket.set(nameProduct, { amount: newAmount, price });
     }
+
+    changeTotalBasket(totalBasket + price * amount);
+    changeBasket(state.basket);
   }
 
   function deleteFromBasket(nameProduct) {
-    if (basket.hasOwnProperty(nameProduct)) {
-      let amountOfDeletedItem =
-        state.basket[nameProduct][1] * state.basket[nameProduct][2];
-      delete state.basket[nameProduct];
-      let basket = state.basket;
-      basket.total = basket.total - amountOfDeletedItem;
-      changeBasket({ ...basket });
+    if (state.basket.has(nameProduct)) {
+      let { amount, price } = state.basket.get(nameProduct);
+      state.basket.delete(nameProduct);
+
+      changeTotalBasket(totalBasket - amount * price);
     }
   }
 
@@ -55,7 +52,11 @@ export function App() {
         <div className="App">
           <nav>
             <MenuBlock handleSwitcherCategory={handleSwitcherCategory} />
-            <Basket basket={state.basket} deleteFromBasket={deleteFromBasket} />
+            <Basket
+              basket={state.basket}
+              deleteFromBasket={deleteFromBasket}
+              totalBasket={totalBasket}
+            />
           </nav>
           <Content
             category={state.showCategory}
--- src/components/basket/Basket.js
+++ src/components/basket/Basket.js
@@ -6,28 +6,28 @@ import { BasketItem } from "./BasketItem";
 import "../../css/Basket.css";
 
 export default function Basket(props) {
-  const { basket, deleteFromBasket } = props;
+  const { basket, deleteFromBasket, totalBasket } = props;
 
   function BasketList() {
     let list = [];
 
-    for (let key in basket) {
-      if (key !== "total") {
+    if (basket.size !== 0) {
+      for (let key of basket) {
         list.push(
           <BasketItem
             key={list.length}
-            name={key}
-            amount={basket[key][1]}
+            name={key[0]}
+            amount={key[1].amount}
             deleteFromBasket={deleteFromBasket}
           />
         );
-      } else if (key === "total" && basket[key] === 0) {
-        list.push(
-          <Typography.Title level={4} key={list.length}>
-            Товара нет.
-          </Typography.Title>
-        );
       }
+    } else {
+      list.push(
+        <Typography.Title level={4} key={list.length}>
+          Товара нет.
+        </Typography.Title>
+      );
     }
 
     return list;
@@ -42,7 +42,7 @@ export default function Basket(props) {
           <span>Кол-во</span>
         </div>
         <div className="basket_list_body">{BasketList()}</div>
-        <h3>Всего: {basket.total}</h3>
+        <h3>Всего: {totalBasket}</h3>
       </div>
     </div>
   );
